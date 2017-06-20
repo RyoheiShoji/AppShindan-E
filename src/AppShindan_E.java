f693acd472f5532d4c67bb8d7d0720420170d8fe
Ryohei Shoji <ryohei.shoji.sys@gmail.com>
2017-06-20 11:17:16 +0900

    質問1の作成 #4

--- src/AppShindan_E.java
+++ src/AppShindan_E.java
@@ -1,18 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class AppShindan_E {
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args){
 		//メイン処理
-
+		situmon1(0);
+		situmon2(0);
+		situmon3(0);
+		situmon4(0);
 		//回答内容で診断結果を考える処理
-
 		//回答結果を出力
 
 	}
 
-	public void situmon1(int a){
+	public static void situmon1(int a){
 		//質問内容を表示
 		//回答内容を表示
 		//ユーザーが答えを入力
+		int x = 0;
+		Random ra = new Random();
+		String[] s = {"男性" ,"女性","その他"};
+		System.out.println("あなたの性別は");
+		System.out.print("(1.男性 , 2.女性 , 3.その他");
+		try{
+			x = sc.nextInt();
+		}catch (NumberFormatException e) {
+			System.out.println("数値が入力されていません");
+			x = 3;
+		}
+		while(0 > x || x > 3 ){
+			System.out.print("1～3のどれかを入力してください");
+			try{
+				x = sc.nextInt();
+			}catch (NumberFormatException e) {
+				System.out.println("数値が入力されていません");
+				x = 3;
+			}
+		}
+		if(ra.nextInt(10) >= 9){
+			System.out.println("あなたはうそつきです。");
+		}else{
+			System.out.println("あなたは" + s[x-1] + "です。");
+		}
 	}
 	public void situmon2(int a){
 		//
@@ -23,5 +53,4 @@ public class AppShindan_E {
 	public void situmon4(int a){
 		//
 	}
-
 }
