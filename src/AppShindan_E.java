af4e8c9e30ee916c72cde952884aed336349e792
shimotai keita <shimotai.keita.sys@gmail.com>
2017-06-20 12:07:25 +0900

    situmon3#3

--- src/AppShindan_E.java
+++ src/AppShindan_E.java
@@ -18,7 +18,16 @@ public class AppShindan_E {
 		//
 	}
 	public void situmon3(int a){
-		//
+		//下平啓太
+		System.out.println("あなたはリーダーになりたい派？メンバー派？1:リーダー2:メンバー3:どっちでもいい");
+		int ansewer = sc.nextInt();
+		if(ansewer == 1){
+			System.out.println("リーダーシップが高い方になるでしょう");
+		}else if(ansewer == 2){
+			System.out.println("協調性が高い方になるでしょう");
+		}else{
+			System.out.println("優柔不断な方なのですね");
+		}
 	}
 	public void situmon4(int a){
 		//
