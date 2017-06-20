0864eaad3ff3f2aa722acaea9d1af9731d49d470
Ryohei Shoji <ryohei.shoji.sys@gmail.com>
2017-06-20 10:13:43 +0900

    first commit

---
+++ src/AppShindan_E.java
@@ -0,0 +1,27 @@
+
+public class AppShindan_E {
+	public static void main(String[] args){
+		//メイン処理
+
+		//回答内容で診断結果を考える処理
+
+		//回答結果を出力
+
+	}
+
+	public void situmon1(int a){
+		//質問内容を表示
+		//回答内容を表示
+		//ユーザーが答えを入力
+	}
+	public void situmon2(int a){
+		//
+	}
+	public void situmon3(int a){
+		//
+	}
+	public void situmon4(int a){
+		//
+	}
+
+}
